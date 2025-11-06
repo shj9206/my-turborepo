adbeafbcdccf62bce57ef5bc3799c2a833d62a88
shj9206 <shj9206@gmail.com>
2025-11-06 12:09:22 +0900

    feat: BookCoverSection Swiper 적용

--- apps/web/service/main/components/BookSection/components/BookCoverSection.tsx
+++ apps/web/service/main/components/BookSection/components/BookCoverSection.tsx
@@ -1,11 +1,25 @@
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
 import { BookCover } from "@repo/ui";
 
-export const BookCoverSection = ({items}: {items: any[]}) => {
+export const BookCoverSection = ({ items }: { items: any[] }) => {
   return (
-    <section className="flex flex-row gap-4">
-    {items.map((item: any) => (
-      <BookCover key={item.id} imageUrl={item.cover} size="sm" />
-    ))}
-  </section>
+    <section className="flex flex-row -mr-6">
+      <Swiper
+        spaceBetween={16}
+        slidesPerView="auto"
+        freeMode={true}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        {items.map((item: any) => (
+          <SwiperSlide key={item.id} style={{ width: "auto" }}>
+            <BookCover imageUrl={item.cover} size="sm" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
   );
 };
