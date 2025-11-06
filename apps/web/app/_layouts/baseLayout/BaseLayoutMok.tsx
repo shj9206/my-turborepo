a68d67c248772c1c25013c09f6dff13c8fc5704b
shj9206 <shj9206@gmail.com>
2025-11-06 22:46:47 +0900

    feat: layout Footer 추가

--- apps/web/app/_layouts/baseLayout/BaseLayoutMok.tsx
+++ apps/web/app/_layouts/baseLayout/BaseLayoutMok.tsx
@@ -1,11 +1,13 @@
 import Header from "@/app/_components/header";
 import { IBaseLayoutProps } from "./interface";
+import { Footer } from "@repo/ui";
 
 export default function BaseLayoutMock({ children }: IBaseLayoutProps) {
   return (
     <section className="w-full mx-auto flex flex-col">
       <Header title="Home" />
       {children}
+      <Footer />
     </section>
   );
 }
