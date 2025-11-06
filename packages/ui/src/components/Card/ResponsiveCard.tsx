178b272b059b72b806e0ce2726a50588decd30a7
shj9206 <shj9206@gmail.com>
2025-11-07 00:22:41 +0900

    feat: 반응형 카드 추가

--- packages/ui/src/components/Card/ResponsiveCard.tsx
+++ packages/ui/src/components/Card/ResponsiveCard.tsx
@@ -14,28 +14,30 @@ export const ResponsiveCard = ({
   link,
 }: IResponsiveCardProps) => {
   return (
-    <div className="mx-auto max-w-md overflow-hidden rounded-xl bg-white shadow-md md:max-w-2xl">
-      <div className="md:flex">
+    <div className="mx-auto max-w-md overflow-hidden rounded-xl bg-white shadow-md md:max-w-6xl h-[500px] md:h-80">
+      <div className="md:flex h-full">
         <div className="md:shrink-0">
           <img
-            className="h-48 w-full object-cover md:h-full md:w-48"
+            className="h-64 w-full object-cover md:h-full md:w-48"
             src={imageUrl}
             alt={title}
           />
         </div>
-        <div className="p-8">
-          <div className="text-sm font-semibold tracking-wide text-indigo-500 uppercase">
+        <div className="p-8 h-[436px] md:h-full overflow-hidden flex flex-col">
+          <div className="text-sm font-semibold tracking-wide text-indigo-500 uppercase line-clamp-1 md:line-clamp-2 overflow-hidden">
             {title}
           </div>
           {link && (
             <a
               href={link}
-              className="mt-1 block text-lg leading-tight font-medium text-black hover:underline"
+              className="mt-1 text-lg tracking-wide font-medium text-black hover:underline line-clamp-1 md:line-clamp-2 overflow-hidden"
             >
               {subTitle}
             </a>
           )}
-          <p className="mt-2 text-gray-500">{description}</p>
+          <p className="mt-2 text-gray-500 line-clamp-5 overflow-hidden">
+            {description}
+          </p>
         </div>
       </div>
     </div>
