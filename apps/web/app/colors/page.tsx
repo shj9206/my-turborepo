d15f27213de6ad1e1f1063bce0abdee8cc77be3a
shj9206 <shj9206@gmail.com>
2025-11-07 11:29:37 +0900

    delete: colors sample page 삭제

--- apps/web/app/colors/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ColorDemo } from "@repo/ui/color-demo";
-
-export default function ColorsPage() {
-  return (
-    <div className="min-h-screen bg-gray-50">
-      <ColorDemo />
-    </div>
-  );
-}
