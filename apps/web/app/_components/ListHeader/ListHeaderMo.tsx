72e50f4953abd3c591e53354161169446d04bcac
shj9206 <shj9206@gmail.com>
2026-01-07 22:53:34 +0900

    feat: listHeader 추가

---
+++ apps/web/app/_components/ListHeader/ListHeaderMo.tsx
@@ -0,0 +1,28 @@
+import { Select } from "@repo/ui";
+import { IListHeaderProps } from "./interface";
+
+export const ListHeaderMo = ({
+  handlePageSizeChange,
+  handleSortChange,
+  sort,
+  maxResults,
+  sortOptions,
+  maxResultsOptions,
+}: IListHeaderProps) => {
+  return (
+    <section className="w-full flex flex-row justify-end items-center border-b-[2px] border-gray-200 pb-6">
+      <div className="flex flex-row items-center gap-2">
+        <Select
+          options={sortOptions}
+          value={sort}
+          onChange={handleSortChange}
+        />
+        <Select
+          options={maxResultsOptions}
+          value={maxResults}
+          onChange={handlePageSizeChange}
+        />
+      </div>
+    </section>
+  );
+};
---
+++ apps/web/app/_components/ListHeader/index.tsx
@@ -0,0 +1,48 @@
+import { useView } from "@/app/_provider/viewProvider";
+import { ListHeaderMo } from "./ListHeaderMo";
+import { ListHeaderPc } from "./ListHeaderPc";
+import { useSearchParams } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
+export const ListHeader = () => {
+  const { IS_MOBILE } = useView();
+  const searchParams = useSearchParams();
+  const sort = searchParams.get("Sort");
+  const maxResults = searchParams.get("MaxResults");
+
+  const router = useRouter();
+  const queryParams = new URLSearchParams(searchParams.toString());
+  const path = usePathname();
+
+  const sortOptions = [
+    { value: "Accuracy", label: "관련순" },
+    { value: "PublishTime", label: "출간일순" },
+    { value: "Title", label: "제목순" },
+    { value: "SalesPoint", label: "판매순" },
+    { value: "CustomerRating", label: "평점순" },
+    { value: "MyReviewCount", label: "리뷰순" },
+  ];
+  const maxResultsOptions = [
+    { value: "20", label: "20개씩 보기" },
+    { value: "40", label: "40개씩 보기" },
+  ];
+  const handlePageSizeChange = (value: string) => {
+    queryParams.set("MaxResults", value);
+    router.push(`${path}?${queryParams.toString()}`);
+  };
+  const handleSortChange = (value: string) => {
+    queryParams.set("Sort", value);
+    router.push(`${path}?${queryParams.toString()}`);
+  };
+
+  const listHeaderProps = {
+    handlePageSizeChange,
+    handleSortChange,
+    sort: sort || "Accuracy",
+    maxResults: maxResults || "20",
+    sortOptions,
+    maxResultsOptions,
+  };
+
+  const Component = IS_MOBILE ? ListHeaderMo : ListHeaderPc;
+  return <Component {...listHeaderProps} />;
+};
