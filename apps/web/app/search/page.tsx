dac3ad41ae9509767f578de9b10dd6140885086d
shj9206 <shj9206@gmail.com>
2026-01-08 01:59:49 +0900

    feat: pc searchList pageNation 추가

--- apps/web/app/search/page.tsx
+++ apps/web/app/search/page.tsx
@@ -6,26 +6,31 @@ import {
 } from "@/service/search/constants/searchApiKey";
 import { buildQueryString } from "@repo/util";
 import { useQuery } from "@tanstack/react-query";
-import { useSearchParams } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { ISearchResponse } from "@/service/search";
 import { SearchList } from "@/service/search/components/SearchList";
 import { ISearchListProps } from "@/service/search/components/SearchList/interface";
 import { ListTab } from "@/app/_components";
 import { SEARCH_TAB_LIST } from "@/service/search/constants/tabList";
+import { PageNation } from "@repo/ui";
 
 export default function Search() {
   const searchParams = useSearchParams();
+  const router = useRouter();
+  const path = usePathname();
   const query = searchParams.get("Query");
   const searchTarget = searchParams.get("SearchTarget");
   const queryType = searchParams.get("QueryType");
   const maxResults = searchParams.get("MaxResults");
   const sort = searchParams.get("Sort");
+  const start = searchParams.get("Start");
   const params = {
     QueryType: queryType || ("Keyword" as const),
     Query: query || "",
     SearchTarget: searchTarget || "",
     MaxResults: maxResults || "20",
     Sort: sort || "Accuracy",
+    Start: start || "1",
   };
 
   const { data, isLoading, error } = useQuery<ISearchResponse>({
@@ -51,11 +56,24 @@ export default function Search() {
     tabList: SEARCH_TAB_LIST(query || ""),
     tagetString: "SearchTarget",
   };
+  const totalPages = Math.ceil(
+    (data?.totalResults || 0) / ((maxResults as unknown as number) || 20)
+  );
 
+  const handlePageChange = (page: number) => {
+    router.push(
+      `${path}?${buildQueryString({ ...params, Start: page.toString() })}`
+    );
+  };
   return (
-    <section className="w-full mx-auto flex flex-col gap-4">
+    <section className="w-full mx-auto flex flex-col gap-4 mb-10">
       <ListTab {...listTabProps} />
       <SearchList {...searchListProps} />
+      <PageNation
+        totalPages={totalPages}
+        currentPage={start as unknown as number}
+        onPageChange={(page: number) => handlePageChange(page)}
+      />
     </section>
   );
 }
