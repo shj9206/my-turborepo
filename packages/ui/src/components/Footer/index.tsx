ce1d4bf8809ce6683fbfd813b208a79218d7c377
shj9206 <shj9206@gmail.com>
2026-01-08 13:11:50 +0900

    refactor: hydration 불일치 오류 해결

--- packages/ui/src/components/Footer/index.tsx
+++ packages/ui/src/components/Footer/index.tsx
@@ -1,15 +1,2 @@
-import { FooterPc } from "./FooterPc";
-import { FooterMo } from "./FooterMo";
-
-import { isMobile } from "../../utils/user-agent";
-
-/**
- * 푸터
- * @returns 푸터 컴포넌트
- * @description 푸터 컴포넌트, PC, MO 구분
- */
-export const Footer = () => {
-  const mobile = isMobile();
-  const Component = mobile ? FooterMo : FooterPc;
-  return <Component />;
-};
+export * from "./FooterMo";
+export * from "./FooterPc";
