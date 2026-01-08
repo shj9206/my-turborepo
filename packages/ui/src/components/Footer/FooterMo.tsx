d71969333f68b04352ec6c6b155c3bbb7429234a
shj9206 <shj9206@gmail.com>
2026-01-08 12:49:55 +0900

    refactor: 접근성 향상을 위한 텍스트 채도 추가

--- packages/ui/src/components/Footer/FooterMo.tsx
+++ packages/ui/src/components/Footer/FooterMo.tsx
@@ -3,13 +3,13 @@ export const FooterMo = () => {
     <footer className="flex flex-col p-8 pb-12 bg-gray-10 gap-5 justify-center items-center w-full">
       <span className="text-sm text-black font-bold">준스코프</span>
       <div className="flex flex-col gap-1 items-center">
-        <span className="text-xs text-gray-500">대표자 : 송화준</span>
-        <span className="text-xs text-gray-500">사업자등록 : 175-22-01436</span>
-        <span className="text-xs text-gray-500">전화번호 : 010-3800-5865</span>
-        <span className="text-xs text-gray-500">
+        <span className="text-xs text-gray-600">대표자 : 송화준</span>
+        <span className="text-xs text-gray-600">사업자등록 : 175-22-01436</span>
+        <span className="text-xs text-gray-600">전화번호 : 010-3800-5865</span>
+        <span className="text-xs text-gray-600">
           이메일 : shj9206@gmail.com
         </span>
-        <span className="text-xs text-gray-500">
+        <span className="text-xs text-gray-600">
           주소 : 서울특별시 강서구 등촌동 660-5
         </span>
       </div>
